1f8c1470329b103a5036b887d5f9fdbcacd4b5d0
Bobby Joe Smith III <bobbyjoesmithiii@MacBook-Pro-145.lan>
2024-08-26 22:42:35 -0700

    Add looping through gallery projects

--- slides.js
+++ slides.js
@@ -1,7 +1,10 @@
 const gallery = document.querySelector('.gallery');
 const slides = gallery.querySelectorAll('.project');
 
+// Where we are in the gridPattern array
 let patternIndex = 0;
+// Where we are in the projectBuffer set
+let projectBufferIndex = 0;
 
 let pattern = [
     [4, 5, 2, 0, 0, 0],
@@ -9,8 +12,18 @@ let pattern = [
     [0, 3, 2, 3, 0, 0],
     [0, 2, 1, 2, 1, 0],
     [0, 0, 2, 4, 3, 2],
-    [0, 0, 0, 1, 1, 1],
+    [0, 0, 0, 1, 1, 1]
 ];
+
+let gridPattern = [
+    [4, 5, 2],
+    [4, 5, 2, 2],
+    [3, 2, 3],
+    [2, 1, 2, 1],
+    [2, 4, 3, 2],
+    [1, 1, 1]
+];
+
 
 const allProjects = [
     { class: '.project-1', title: 'Project One Title', pos: 0, src: 'Assets/Projects/watch_the_rubble_01.jpg', keywords: ['Graphic Design', 'Photography', 'Art', 'Cover'] },
@@ -28,13 +41,109 @@ const allProjects = [
 
 let projectBuffer = new Set();
 
-const currentSlides = [];
-
 const selectedKeyWords = ['Cover'];
 
 let timeout;
 
+
+const populateProjectBuffer = () => {
+    allProjects.forEach((project) => {
+        selectedKeyWords.forEach((key) => {
+            if (project.keywords.includes(key)) {
+                projectBuffer.add(project);
+            }
+        })
+    })
+}
+
+populateProjectBuffer();
+
+const patternGenerator = (set) => {
+    // Clear projectSequence array
+    let projectSequence = [];
+    let setSize = set.size;
+    // Add a sum total of zeros to projectSequence[] equal to projectBuffer.length. 
+    while (projectSequence.length < setSize) {
+        projectSequence.push(0);
+    }
+
+    // splice every entry at gridPattern[patternIndex] into projectSequence[] at projectBufferIndex, replacing the numbers that are currently there
+    // if you reach the end of the buffer, then have the pattern loop to the beginning
+
+    let pattern = gridPattern[patternIndex];
+    console.log(pattern);
+    console.log(pattern.length);
+    console.log(`patternIndex = ${patternIndex}`);
+
+    for (let i = 0; i < pattern.length; i++) {
+        console.log('Not on last project');
+        projectSequence.splice(projectBufferIndex + i, 1, pattern[i]);
+    }
+
+    
+    // iterate projectBufferIndex only if the current sequence has 4 fractions or is last sequence in gridPattern
+    if (pattern.length === 4 || patternIndex === gridPattern.length - 1) {   
+        projectBufferIndex++;
+    } 
+
+    if (projectBufferIndex === projectSequence.length) {
+        projectBufferIndex = 0;
+    }
+
+    if (projectSequence.length - pattern.length < projectBufferIndex) {
+        console.log('Last project reached, looping back.');
+        projectBufferIndex = 0;
+        patternIndex = -1;
+        // projectSequence.splice(projectBufferIndex, 3, 4, 5, 2);
+    }
+
+    // patternIndex++;
+    return projectSequence;
+
+}
+
+patternGenerator(projectBuffer);
+
+
+
+const createProjectElements = () => {
+    gallery.style.gridTemplateColumns = patternGenerator(projectBuffer)
+        .map((p) => {
+            return `${p}fr`
+        })
+        .join(' ');
+
+    for (let project of projectBuffer) {
+        // create the elements
+        const newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'project');
+        const newH2 = document.createElement('h2');
+        newH2.textContent = project.title;
+        
+        const newFigure = document.createElement('figure');
+        
+        const newImg = document.createElement('img');
+        newImg.setAttribute('src', project.src)
+        
+        const newFigcaption = document.createElement('figcaption');
+        newFigcaption.setAttribute('class', 'keywords');
+        newFigcaption.textContent = `#${project.keywords.join(' #')}`;
+        
+        // construct the element and append to dom
+        const gallery = document.querySelector('.gallery');
+        gallery.append(newDiv);
+        newDiv.append(newH2);
+        newDiv.append(newFigure);
+        newFigure.append(newImg);
+        newFigure.append(newFigcaption);
+
+    }
+}
+
+createProjectElements();
+
 const hideProjects = () => {
+    console.log(slides);
     slides.forEach((slide, slideIndex) => {
         if (pattern[patternIndex][slideIndex] === 0) {
             slide.classList.add('hide')
@@ -46,15 +155,17 @@ const hideProjects = () => {
 
 hideProjects();
 
-const nextSlide = () => {
-    patternIndex += 1
-    patternIndex %= pattern.length
 
-    gallery.style.gridTemplateColumns = pattern[patternIndex]
+
+const nextSlide = () => {
+    // Iterate patternIndex and loop if at the end of gridPattern array
+    (patternIndex < gridPattern.length - 1) ? patternIndex++ : patternIndex = 0;
+
+    gallery.style.gridTemplateColumns = patternGenerator(projectBuffer)
         .map((p) => {
             return `${p}fr`
         })
-        .join(' ')
+        .join(' ');
 
     hideProjects();
 
@@ -62,20 +173,8 @@ const nextSlide = () => {
     // timeout = setTimeout(nextSlide, 2000)
 }
 
-const populateProjectBuffer = () => {
-    allProjects.forEach((project) => {
-        console.log(`project = ${project}`)
-        selectedKeyWords.forEach((key) => {
-            console.log(`key = ${key}`)
-            if (project.keywords.includes(key)) {
-                projectBuffer.add(project);
-            }
-        })
-    })
-    console.log(projectBuffer);
-}
 
-populateProjectBuffer();
+
 
 gallery.addEventListener('click', nextSlide);
 
