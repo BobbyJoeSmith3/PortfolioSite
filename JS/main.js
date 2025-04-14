c8ac0f4b212a7d646a05cf1cd79d31fa675702b6
BOBBY JOE SMITH III <bobbyjoesmithiii@BOBBYs-Laptop.lan>
2025-04-14 16:42:39 -0700

    Add fullscreen functionality.

--- JS/main.js
+++ JS/main.js
@@ -18,22 +18,27 @@ let carousel = document.querySelector('.main-carousel');
 
 const flkty = new Flickity(carousel, {
     // options
-    // imagesLoaded: true,
-    lazyload: true,
+    imagesLoaded: true,
     draggable: '>1',
     autoPlay: true,
     cellAlign: 'left',
     contain: true,
     wrapAround: true,
     prevNextButtons: true,
-    pageDots: true
+    pageDots: true,
+    fullscreen: true
     // setGallerySize: false
 });
+
+/* ======================
+Carousel Navigation UI
+======================== */
 
 const car_nav = document.querySelector('.carousel-nav');
 let prev_btn = document.querySelector('.previous');
 let next_btn = document.querySelector('.next');
 let page_dots = document.querySelector('.flickity-page-dots');
+let fullscreen_btn = document.querySelector('.flickity-fullscreen-button');
 
 // Modify Carousel Navigation
 const prev_txt = document.createElement('span');
@@ -46,6 +51,8 @@ next_txt.textContent = "next";
 next_btn.insertBefore(next_txt, next_btn.querySelector('.flickity-button-icon'));
 car_nav.appendChild(next_btn);
 car_nav.insertBefore(page_dots, next_btn);
+// Move fullscreen button into carousel
+carousel.insertBefore(fullscreen_btn, carousel.firstChild);
 
 
 
