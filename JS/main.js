864ce105c1c499b5022789e8e22c9bd9c386958c
BOBBY JOE SMITH III <bobbyjoesmithiii@MacBookPro.lan>
2025-04-20 01:03:52 -0700

    Use imagesLoaded package and flickity API to create project filtering engine for carousel

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,9 @@
- /* ======================
-Initialize Flickity
+/* ======================
+Initializations
 ======================== */
-// Needs to initialize while page is loading
 
+// Initialize flickity
+// Needs to initialize while page is loadin
 let $carousel = $('.main-carousel');
 
 $carousel.flickity({
@@ -28,13 +29,42 @@ $(function() {
     const $contact_btn = $('.contact_btn');
 
     // Rotate contact info modal button on click
-    // $contact_btn.addEventListener('click', () => {
-    //     $contact_btn.firstChild.classList.toggle('active');
-    // });
-
     $contact_btn.click( ()=> {
         $contact_btn.children('svg').toggleClass('active');
     });
+
+    $('.filters').on('click', 'button', function() {
+        let filterValue = $(this).attr('data-filter');
+        console.log(filterValue);
+        let $cellElements = $carousel.flickity('getCellElements');
+        $($cellElements).each( function () {
+            $carousel.flickity('remove', $(this));
+        })
+        filterProjects(filterValue);
+        $('.flickity-viewport').imagesLoaded().always(function() {
+            resize_carousel();
+        });
+        
+    });
+
+    function filterProjects (filterValue) {
+        for (let project of allProjects) {
+            if (project.keywords.includes(filterValue)) {
+                let proj = document.createElement('div');
+                $(proj).addClass(`project carousel-cell ${project.keywords.join(' ')}`);
+                let htmlString = `<h2>${project.title}</h2><figure><img class="project carousel-cell-image ${project.keywords.join(" ")}" src="${project.src}"><figcaption class="keywords">#${project.keywords.join(" #")}</figcaption></figure>`;
+                $(proj).append(htmlString);
+                $carousel.flickity('append', proj);
+            }
+        }
+        
+    }
+    
+    function resize_carousel() {
+        $carousel.flickity('reloadCells');
+        $carousel.flickity('resize');
+        $carousel.flickity('reposition');
+    }
 
 
     /* ======================
--- JS/project_db.js
+++ JS/project_db.js
@@ -2,17 +2,17 @@
     Project Database
 =============================== */
 const allProjects = [
-    { class: '.project-1', title: 'Project One Title', pos: 0, src: 'Assets/Projects/watch_the_rubble_01.jpg', keywords: ['Graphic Design', 'Photography', 'Art', 'TestA'] },
-    { class: '.project-2', title: 'Project Two Title', pos: 1, src: 'Assets/Projects/watch_the_rubble_03.jpg', keywords: ['Graphic Design', 'Photography', 'Art', 'TestA', 'TestD'] },
-    { class: '.project-3', title: 'Project Three Title', pos: 2, src: 'Assets/Projects/Aglaura-1.jpg', keywords: ['Graphic Design', 'Photography', 'TestA'] },
-    { class: '.project-4', title: 'Project Four Title', pos: 3, src: 'Assets/Projects/Aglaura-2.jpg', keywords: ['Graphic Design', 'Photography', 'TestA'] },
-    { class: '.project-5', title: 'Project Five Title', pos: 4, src: 'Assets/Projects/watch_the_rubble_04.jpg', keywords: ['Graphic Design', 'Photography', 'Art', 'TestA'] },
-    { class: '.project-6', title: 'Project Six Title', pos: 5, src: 'Assets/Projects/Aglaura-3.jpg', keywords: ['Graphic Design', 'Photography', 'TestB'] },
-    { class: '.project-7', title: 'Project Seven Title', pos: 6, src: 'Assets/Projects/Baltimore-01.jpg', keywords: ['Photography', 'TestB'] },
-    { class: '.project-8', title: 'Project Eight Title', pos: 7, src: 'Assets/Projects/Baltimore-02.jpg', keywords: ['Photography', 'TestB'] },
-    { class: '.project-9', title: 'Project Nine Title', pos: 8, src: 'Assets/Projects/Baltimore-03.jpg', keywords: ['Photography', 'TestB'] },
-    { class: '.project-10', title: 'Project Ten Title', pos: 9, src: 'Assets/Projects/Aglaura-4.jpg', keywords: ['Graphic Design', 'Photography', 'TestB'] },
-    { class: '.project-11', title: 'Project Eleven Title', pos: 10, src: 'Assets/Projects/Baltimore-04.jpg', keywords: ['Photography', 'TestC'] },
-    { class: '.project-12', title: 'Project Twelve Title', pos: 11, src: 'Assets/Projects/Baltimore-05.jpg', keywords: ['Photography', 'TestC'] },
-    { class: '.project-13', title: 'Project Thirteen Title', pos: 12, src: 'Assets/Projects/Baltimore-06.jpg', keywords: ['Photography', 'TestC'] }
+    { class: '.project-1', title: 'Project One Title', pos: 0, src: 'Assets/Demo/SMSL-1.jpg', keywords: ['Graphic Design', 'Photography', 'Art', 'TestA'] },
+    { class: '.project-2', title: 'Project Two Title', pos: 1, src: 'Assets/Demo/SMSL-2.jpg', keywords: ['Graphic Design', 'Photography', 'Art', 'TestA', 'TestD'] },
+    { class: '.project-3', title: 'Project Three Title', pos: 2, src: 'Assets/Demo/SMSL-3.jpg', keywords: ['Graphic Design', 'Photography', 'TestA'] },
+    { class: '.project-4', title: 'Project Four Title', pos: 3, src: 'Assets/Demo/SMSL-4.jpg', keywords: ['Graphic Design', 'Photography', 'TestA'] },
+    { class: '.project-5', title: 'Project Five Title', pos: 4, src: 'Assets/Demo/SMSL-5.jpg', keywords: ['Graphic Design', 'Photography', 'Art', 'TestA'] },
+    { class: '.project-6', title: 'Project Six Title', pos: 5, src: 'Assets/Demo/SMSL-6.jpg', keywords: ['Graphic Design', 'Photography', 'TestB'] },
+    { class: '.project-7', title: 'Project Seven Title', pos: 6, src: 'Assets/Demo/SMSL-7.jpg', keywords: ['Photography', 'TestB'] },
+    { class: '.project-8', title: 'Project Eight Title', pos: 7, src: 'Assets/Demo/SMSL-8.jpg', keywords: ['Photography', 'TestB'] },
+    { class: '.project-9', title: 'Project Nine Title', pos: 8, src: 'Assets/Demo/SMSL-9.jpg', keywords: ['Photography', 'TestB'] },
+    { class: '.project-10', title: 'Project Ten Title', pos: 9, src: 'Assets/Demo/SMSL-10.jpg', keywords: ['Graphic Design', 'Photography', 'TestB'] },
+    { class: '.project-11', title: 'Project Eleven Title', pos: 10, src: 'Assets/Demo/SMSL-11.jpg', keywords: ['Photography', 'TestC'] },
+    { class: '.project-12', title: 'Project Twelve Title', pos: 11, src: 'Assets/Demo/SMSL-12.jpg', keywords: ['Photography', 'TestC'] },
+    { class: '.project-13', title: 'Project Thirteen Title', pos: 12, src: 'Assets/Demo/SMSL-13.jpg', keywords: ['Photography', 'TestC'] }
 ];
